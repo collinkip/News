93204aafe5dc24ed6841bfbec6368e971cea115f
collins <collinskiplangat3@gmail.com>
2022-11-29 09:06:25 +0300

    Added a upload content page

--- app/src/main/java/com/example/news/Upload.java
+++ app/src/main/java/com/example/news/Upload.java
@@ -2,13 +2,28 @@ package com.example.news;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Upload extends AppCompatActivity {
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+    }
+
+    public void upload(View view) {
+
+    }
+
+    public void getImage(View view) {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,100);
     }
 }
